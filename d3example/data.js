feeef8b9e6078afb236ee90d9778bbe2ab068b47
xiaoyunhu <huxiaoyunhust@gmail.com>
2015-01-11 16:17:40 +0800

    test git sth
    d3 example about how to use forcelayout and geopath

---
+++ d3example/data.js
@@ -0,0 +1,118 @@
+data = {
+	"nodes":[{
+		"index": 0, 
+		"name":'JJH',
+		"fixed":true,
+		"type":'sc',
+		"area":'hd',
+		"x":350,
+		"y":350		
+	},{
+		"index": 1,
+		"name":"NJ02",
+		"fixed":true,
+		"type":'sc',
+		"area":'hd',		
+		"x":450,
+		"y":350				
+	},{
+		"index": 2,
+		"name":"BB",
+		"fixed":true,
+		"type":'sc',
+		"area":'hb',		
+		"x":350,
+		"y":450				
+	},{
+		"index": 3,
+		"name":"M1",
+		"fixed":true,
+		"type":'sc',
+		"area":'hb',		
+		"x":450,
+		"y":450				
+	},{
+		"index": 4,
+		"name":"SZWG",
+		"fixed":true,
+		"type":'wh',
+		"area":'hd',
+		"x":250,
+		"y":250									
+	},{
+		"index": 5,
+		"name":"SZJJH",
+		"fixed":true,
+		"type":'wh',
+		"area":'hd',
+		"x":550,
+		"y":250								
+	},{
+		"index": 6,
+		"name":"TC",
+		"fixed":true,
+		"type":'wh',
+		"area":'hb',
+		"x":250,
+		"y":550								
+	},{
+		"index": 7,
+		"name":"DBL",
+		"fixed":true,
+		"type":'wh',
+		"area":'hb',
+		"x":550,
+		"y":550									
+	},
+	],
+	"links":[{
+      	"source": 0, 
+      	"target": 2, 
+      	"weight": 500		
+	},{
+      	"source": 0, 
+      	"target": 3, 
+      	"weight": 500			
+	},{
+      	"source": 1, 
+      	"target": 2, 
+      	"weight": 500		
+	},{
+      	"source": 1, 
+      	"target": 3, 
+      	"weight": 500		
+	},{
+      	"source": 4, 
+      	"target": 0, 
+      	"weight": 120		
+	},{
+      	"source": 4, 
+      	"target": 1, 
+      	"weight": 200		
+	},{
+      	"source": 5, 
+      	"target": 0, 
+      	"weight": 220		
+	},{
+      	"source": 5, 
+      	"target": 1, 
+      	"weight": 240		
+	},{
+      	"source": 6, 
+      	"target": 2, 
+      	"weight": 60		
+	},{
+      	"source": 6, 
+      	"target": 3, 
+      	"weight": 460		
+	},{
+      	"source": 7, 
+      	"target": 2, 
+      	"weight": 160		
+	},{
+      	"source": 7, 
+      	"target": 3, 
+      	"weight": 80		
+	},
+	]
+}
